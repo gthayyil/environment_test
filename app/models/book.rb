32174563c3e5fb5e81cfb2afea1251de2e18012b
root <root@localhost>
2022-02-11 22:10:49 +0000

    multu-book commit

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,7 +1,8 @@
 class Book < ApplicationRecord
+    has_many :user
     validates :title, presence: true
     validates :author, presence: true
     validates :price, presence: true
     validates :published_date, presence: true
-
+   
 end
